c11685396e1f60f281e0d05a0841e96638c2c09b
Kenneth Ponson <kenneth@pgs.local>
2026-06-03 08:55:04 -0400

    allow mpgaruba images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+﻿import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "www.mpgaruba.com", pathname: "/storage/app/uploads/**" },
+      { protocol: "https", hostname: "mpgaruba.com", pathname: "/storage/app/uploads/**" },
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "yvkovfjdhojhzjctnbgl.supabase.co", pathname: "/storage/v1/object/public/**" }
+    ]
+  }
 };
 
 export default nextConfig;
